4f908fd461e72a0cee853935c2811372b70793cf
aleksandrgovorin <govorinsasha0@gmail.com>
2024-09-18 21:32:23 +0200

    Commit 2.2.1

--- src/main/java/com/sky/hogwarts/Scripts/scripts422.sql
+++ src/main/java/com/sky/hogwarts/Scripts/scripts422.sql
@@ -9,14 +9,7 @@ CREATE TABLE Car (
     id BIGINT PRIMARY KEY AUTOINCREMENT,
     brand VARCHAR(255) NOT NULL,
     model VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
-);
-
-
-CREATE TABLE PersonCar (
-    person_id BIGINT,
-    car_id BIGINT,
-    PRIMARY KEY (person_id, car_id),
-    FOREIGN KEY (person_id) REFERENCES Person(id),
-    FOREIGN KEY (car_id) REFERENCES Car(id)
+    price DECIMAL(10, 2) NOT NULL,
+    person_id INT,
+    CONSTRAINT foreignKeyOwner FOREIGN KEY (person_id) REFERENCES Person(id)
 );
